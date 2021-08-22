1d112533a27c36481391352ca5ca35e0e6484a78
Divyanshu Patel <divyansh94.patel@gmail.com>
2021-08-22 16:14:37 +0530

    starter :)

---
+++ Codechef/Large_Square.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+	while(t--){
+
+	    long int a,b,sqr,final;
+
+	    cin>>a>>b;
+
+        sqr = (sqrt(a));
+        final = sqr*b;
+
+        cout<<final<<endl;
+	}
+	return 0;
+}
