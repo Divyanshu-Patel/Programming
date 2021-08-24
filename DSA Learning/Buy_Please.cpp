1b91a6076462f09068ed2e137e6f82ea96132793
Divyanshu Patel <divyansh94.patel@gmail.com>
2021-08-24 13:00:00 +0100

    DS1

---
+++ DSA Learning/Buy_Please.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b, x, y;
+    int sum = 0;
+    cin>>a>>b>>x>>y;
+
+    sum = (a*x) + (b*y);
+
+    cout << sum << endl;
+
+    return 0;
+}
