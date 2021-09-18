96ecbb8a9a098e85fa8ec5c2df0276c928955e98
Divyanshu Patel <divyansh94.patel@gmail.com>
2021-09-18 13:00:00 +0100

    B :)

---
+++ Codechef - Prac/Finding_Square_Roots.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int ans;
+        ans=sqrt(n);
+        cout<<ans<<endl;
+    }
+	return 0;
+}
