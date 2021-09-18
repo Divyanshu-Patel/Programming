f4841e3d79cc36ff2a66749bcc037503d009e031
Divyanshu Patel <divyansh94.patel@gmail.com>
2021-09-18 13:00:00 +0100

    B :)

---
+++ Codechef - Prac/Second_Largest.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int a, b, c;
+        cin>>a>>b>>c;
+        
+        if(a>b && a>c){
+            if(b>c){
+                cout<<b<<endl;
+            }
+
+            else{
+                cout<<c<<endl;
+            }
+            
+        }
+
+        else if (b>a && b>c){
+            if(a>c){
+                cout<<a<<endl;
+            }
+
+            else{
+                cout<<c<<endl;
+            }
+        }
+
+        else if (c>b && c>a){
+            if(b>a){
+                cout<<b<<endl;
+            }
+
+            else{
+                cout<<a<<endl;
+            }
+        }
+        
+
+    }
+
+    return 0;
+}
