8cca15b6150d55aa498bd3c0988deaa7cee86d25
Divyanshu Patel <divyansh94.patel@gmail.com>
2021-09-19 13:00:00 +0100

    B :)

---
+++ Codechef - Prac/Packaging_Cupcakes.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n, ans;
+        cin>>n;
+
+        ans = n/2 + 1;
+
+        cout<<ans<<endl;
+        
+    }
+
+    return 0;
+}
