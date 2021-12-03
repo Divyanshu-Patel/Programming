58fce37a2af617ad6d67d113cf04e9e771d5ccbf
Divyanshu Patel <divyansh94.patel@gmail.com>
2021-12-03 14:00:00 +0100

    Contest

---
+++ Contest/Game_between_friends.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+
+        if(a>=b){
+            b = b+c;
+
+            if(a>=b){
+                b = b+d;
+            }
+
+            else{
+                a = a+d;
+            }
+
+            if(a>=b){
+                cout<<"N"<<endl;
+            }
+
+            else{
+                cout<<"S"<<endl;
+            }
+        }
+
+        else{
+            a = a+c;
+
+            if(a>=b){
+                b = b+d;
+            }
+
+            else{
+                a = a+d;
+            }
+
+            if(a>=b){
+                cout<<"N"<<endl;
+            }
+
+            else{
+                cout<<"S"<<endl;
+            }
+        }
+
+    }
+
+    return 0;
+}
