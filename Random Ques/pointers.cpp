cf5c6c6ab7c001afd093138c7a9981a5a68c0d63
Divyanshu Patel <divyansh94.patel@gmail.com>
2022-06-17 22:45:29 +0530

    xd

---
+++ Random Ques/pointers.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int *ptrs;
+    ptrs = &n;
+
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    for(int i = 0;i<n; i++){
+        cout<<*(arr+i)<<endl;
+    }
+
+    cout<<*ptrs<<endl;
+
+    return 0;
+}
