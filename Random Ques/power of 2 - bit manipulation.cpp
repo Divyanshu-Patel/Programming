159a3f205af614488c4528148be2f791dbda0872
Divyanshu Patel <divyansh94.patel@gmail.com>
2022-06-18 21:28:42 +0530

    xd

---
+++ Random Ques/power of 2 - bit manipulation.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    if((n&(n-1)) == 0){
+        cout<<"yes"<<endl;
+    }
+
+    else{
+        cout<<"no"<<endl;
+    }
+
+    return 0;
+}
