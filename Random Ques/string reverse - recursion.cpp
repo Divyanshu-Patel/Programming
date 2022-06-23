1b81f48081da7b7862e35e5ce01c1df7c2866a25
Divyanshu Patel <divyansh94.patel@gmail.com>
2022-06-24 00:49:41 +0530

    xd

---
+++ Random Ques/string reverse - recursion.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rev(string s){
+
+    if(s.length() == 0){
+        return;
+    }
+
+    string ros = s.substr(1);
+    rev(ros);
+    cout<<s[0];
+
+}
+
+int main(){
+
+    string s;
+    cin>>s;
+
+    rev(s);
+
+    return 0;
+}
