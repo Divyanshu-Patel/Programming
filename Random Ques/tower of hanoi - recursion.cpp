0ad4576d1dfebde14877cd49f734ec44e994a805
Divyanshu Patel <divyansh94.patel@gmail.com>
2022-06-25 20:56:50 +0530

    xd

---
+++ Random Ques/tower of hanoi - recursion.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void tower(int n, char src, char help, char dest){
+
+    if(n==0){return;}
+
+    tower(n-1, src, dest, help);
+    cout<<"Move from"<<" "<<src<<" "<<"to"<<" "<<dest<<endl;;
+    tower(n-1, help, src, dest);
+
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    tower(n, 'A', 'B', 'C');
+
+    return 0;
+}
