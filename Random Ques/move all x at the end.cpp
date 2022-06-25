de1216e0da5c751e8fc98f7892d9112741965917
Divyanshu Patel <divyansh94.patel@gmail.com>
2022-06-25 20:57:07 +0530

    xd

---
+++ Random Ques/move all x at the end.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string moveall(string s){
+
+    if(s.length()==0){
+        return "";
+    }
+
+    char ch=s[0];
+    string ans = moveall(s.substr(1));
+
+    if(ch=='x'){
+        return ans+ch;
+    }
+
+    return ch+ans;
+
+}
+
+int main(){
+
+    string s;
+    cin>>s;
+
+    cout<<moveall(s);
+
+    return 0;
+}
