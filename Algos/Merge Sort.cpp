adc37e7fdcad74fe96e0a54e2995e50f2997f3bd
Divyanshu Patel <divyansh94.patel@gmail.com>
2022-07-12 23:29:04 +0530

    Merge Sort

--- Algos/Merge Sort.cpp
+++ Algos/Merge Sort.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void ort(int arr[], int left, int mid, int right){
+void sort(int arr[], int left, int mid, int right){
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
