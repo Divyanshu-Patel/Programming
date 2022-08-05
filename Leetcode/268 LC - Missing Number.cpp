9b68d8e4c412c5ceb478b7b1c512cfb34e68143a
Divyanshu Patel <divyansh94.patel@gmail.com>
2022-08-06 02:40:57 +0530

    LC

---
+++ Leetcode/268 LC - Missing Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size()+1;
+        vector<int> mpx(n,0);
+        vector<int> ans;
+        
+        for(int i=0; i<nums.size(); i++){
+            mpx[nums[i]]++;
+        }
+        
+        for(int i=0; i<=nums.size(); i++){
+            if(mpx[i]==0){
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+};
